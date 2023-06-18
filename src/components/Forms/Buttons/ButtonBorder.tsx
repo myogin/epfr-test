47c49bb0f63f8de3bef211fa7bd14fd641a16a81
habibkoe <habibsantri@gmail.com>
2023-06-19 06:49:25 +0800

    update

--- src/components/Forms/Buttons/ButtonBorder.tsx
+++ src/components/Forms/Buttons/ButtonBorder.tsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React from "react";
 
 interface Props {
-    children?: React.ReactNode;
-    onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-    className?: any
-  }
+  children?: React.ReactNode;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  className?: any;
+}
 
 const ButtonBorder = (prop: Props) => {
   return (
-    <button className={`flex ${prop.className ? "" : "justify-start"} gap-2 px-3 py-2 text-sm ${prop.className ? "" : "text-gray-light border border-gray-soft-strong"} rounded-lg ${prop.className}`} onClick={prop.onClick}>
+    <button
+      className={`flex ${
+        prop.className ? "" : "justify-start"
+      } gap-2 px-3 py-2 text-sm ${
+        prop.className ? "" : "text-gray-light border border-gray-soft-strong"
+      } rounded-lg ${prop.className}`}
+      onClick={prop.onClick}
+    >
       {prop.children}
     </button>
   );
 };
 
-export default ButtonBorder
+export default ButtonBorder;
