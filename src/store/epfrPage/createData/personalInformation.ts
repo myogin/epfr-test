c746aed17c2ea666d8e247e319128c187c17b367
habibkoe <habibsantri@gmail.com>
2023-06-19 08:51:03 +0800

    update immer

--- src/store/epfrPage/createData/personalInformation.ts
+++ src/store/epfrPage/createData/personalInformation.ts
@@ -1,41 +1,89 @@
-import { PersonalInformation } from "@/models/SectionOne"; 
-import {create} from "zustand";
+import { SectionOne } from "@/models/SectionOne";
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 
-interface PersonalState {
-  dataPersonal: PersonalInformation[];
-  addData: (description: string) => void;
-  removeData: (id: string) => void;
-  toggleCompletedState: (id: string) => void;
-}
+type Actions = {
+  handleClient: (params: []) => any;
+  handleDependent: (params: []) => any;
+  handleAccompaniment: (params: []) => any;
+  handleTrustedIndividuals: (params: []) => any;
+};
 
-export const useStore = create<PersonalState>((set) => ({
-    // initial state
-    dataPersonal: [],
-    // methods for manipulating state
-    addData: (description: string) => {
-      set((state) => ({
-        todos: [
-          ...state.todos,
-          {
-            id: uuidv4(),
-            description,
-            completed: false,
-          } as Todo,
-        ],
-      }));
+const personalInformation = create(
+  devtools<SectionOne & Actions>((set, get) => ({
+    ownerId: 0,
+    type: 0,
+    id: 0,
+    clientInfo: [
+      {
+        clientTitle: "",
+        clientName: "",
+        otherName: "",
+        relationship: "",
+        gender: "",
+        passportNo: "",
+        nationality: "",
+        residency: "",
+        residencyTwo: "",
+        residencyOther: "",
+        dateOfBirth: "",
+        marital: "",
+        smoker: "",
+        employmentStatus: "",
+        occupation: "",
+        companyName: "",
+        businessNature: "",
+        annualIncome: "",
+        contactHome: "",
+        contactMobile: "",
+        contactOffice: "",
+        contactFax: "",
+        email: "",
+        residentialAddr: "",
+        mailingAddr: "",
+      },
+    ],
+    dependant: [
+      {
+        name: "test",
+        relationship: "SON",
+        dateOfBirth: "10 June 2010",
+        age: 13,
+        gender: "1",
+        year: "2",
+      },
+    ],
+    accompaniment: [
+      {
+        age: 0,
+        english_spoken: "",
+        english_written: "",
+        education_level: "",
+      },
+    ],
+    trustedIndividuals: {
+      condition1: false,
+      condition2: false,
+      trustedEmail: "",
+      nameOfTrustedIndividual: "",
+      passportNo: "",
+      relationship: "",
+      languageUsed: "",
+      contactNumber: "",
+      englishLevel1: 0,
+      englishLevel2: 0,
+      educationLevel: 0,
+      ageLevel: 0,
+      declaration: 0,
     },
-    removeData: (id) => {
-      set((state) => ({
-        todos: state.todos.filter((todo) => todo.id !== id),
-      }));
-    },
-    toggleCompletedState: (id) => {
-      set((state) => ({
-        todos: state.todos.map((todo) =>
-          todo.id === id
-            ? ({ ...todo, completed: !todo.completed } as Todo)
-            : todo
-        ),
-      }));
-    },
-  }));
+    issues: [],
+    reviewDate: "",
+    status: 0,
+    handleClient: (params: number) => set(() => ({ dataId: params })),
+    handleDependent: () => set(() => ({ dataId: 0 })),
+    handleAccompaniment: () => set(() => ({ dataId: 0 })),
+    handleTrustedIndividuals: () => set(() => ({ dataId: 0 })),
+  }))
+);
+
+export const usePersonalInformation = personalInformation;
