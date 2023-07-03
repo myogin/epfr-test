67abc0062b37a0fd676be4e4183c8d3111a26d46
Jangkrek EleventTweleve <jangkrekeleventtweleve@Jangkreks-MacBook-Pro.local>
2023-07-03 17:45:44 +0700

    [UP] Done Section 8

--- src/components/Attributes/Rows/Grids/RowSixGrid.tsx
+++ src/components/Attributes/Rows/Grids/RowSixGrid.tsx
@@ -3,12 +3,11 @@ import React from 'react'
 interface Props {
     children?: React.ReactNode;
     className?: string;
-    key?: any;
   }
 
-const RowSixGrid = ({ children, className, key }: Props) => {
+const RowSixGrid = ({ children, className}: Props) => {
   return (
-    <div key={key} className={`grid ${className} grid-cols-1 gap-4 mb-5 lg:grid-cols-6 sm:grid-cols-3 md:grid-cols-3`}>
+    <div className={`grid ${className} grid-cols-1 gap-4 mb-5 lg:grid-cols-6 sm:grid-cols-3 md:grid-cols-3`}>
       {children}
     </div>
   )
