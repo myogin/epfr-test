c476ceb46d7fd840af37a1b381ced4f3fe4aa71b
habibkoe <habibsantri@gmail.com>
2023-08-06 07:54:14 +0800

    update

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,65 +1,9 @@
-import React, { useEffect } from "react";
-import Head from "next/head";
-import { Page } from "@/pages/_app";
-import AppLayout from "@/components/Layouts/AppLayout";
-import GlobalCard from "@/components/Attributes/Cards/GlobalCard";
-import TitleMedium from "@/components/Attributes/Typography/TitleMedium";
-import { useRouter } from "next/router";
-import CheckboxCircleFillIcon from "remixicon-react/CheckboxCircleFillIcon";
-import ButtonGreenMedium from "@/components/Forms/Buttons/ButtonGreenMedium";
-import ButtonBorderMedium from "@/components/Forms/Buttons/ButtonBorderMedium";
-import { signIn, useSession } from "next-auth/react";
-import { useLoginData } from "@/store/login/logindata";
-import { localOwnerId, localPfrId, localToken, localType } from "@/libs/helper";
+import React from 'react'
 
-const EpfrLogin: Page = () => {
-  const router = useRouter();
-  const { status } = useSession();
-  const { setLogin, token } = useLoginData();
+const index = () => {
+  return (
+    <div>index</div>
+  )
+}
 
-  const loginTest = async (token1: any, typeEpfr: number) => {
-    if (token1) {
-      const result = await signIn("credentials", {
-        token: token1,
-        redirect: false,
-      });
-
-      if (result?.status == 200) {
-        if(typeEpfr !== null) {
-          let typeString = typeEpfr !== null ? typeEpfr == 1 ? "single" : "joint" : ""
-          router.push("/create/"+typeString+"#section-1");
-        }else {
-          router.push("/epfr");
-        }
-      } else {
-        router.push("/unauthorized");
-      }
-    }
-  };
-
-  useEffect(() => {
-    let tokenFix = localToken();
-    let ownerFix = localOwnerId();
-    let pfrFix = localPfrId();
-    let typeEpfrFix = localType();
-
-    let localT = router.query.token == null || router.query.token == undefined ? tokenFix : router.query.token;
-    let pfrId = router.query.pfrId == null || router.query.pfrId == undefined ? pfrFix : router.query.pfrId;
-    let ownerId = router.query.ownerId == null || router.query.ownerId == undefined ? ownerFix : router.query.ownerId;
-    let typeEpfr = router.query.typeEpfr == null || router.query.typeEpfr == undefined ? typeEpfrFix : router.query.typeEpfr;
-
-    if (localT == undefined || localT == null) {
-      router.push("/unauthorized");
-    }
-    setLogin(localT, ownerId, Number(pfrId), typeEpfr);
-    loginTest(localT,typeEpfr);
-  });
-
-  return <div>...Loading</div>;
-};
-
-EpfrLogin.getLayout = function getLayout(content: any) {
-  return <AppLayout>{content}</AppLayout>;
-};
-
-export default EpfrLogin;
+export default index
