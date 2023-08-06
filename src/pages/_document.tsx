8ec4ce9954ea312387a96369ac99723cac4aed71
habibkoe <habibsantri@gmail.com>
2023-08-06 13:18:28 +0800

    update document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta
-          http-equiv="Content-Security-Policy"
+          httpEquiv="Content-Security-Policy"
           content="upgrade-insecure-requests"
         />
         <meta
