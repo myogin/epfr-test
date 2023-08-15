7bc08b381e4b64ce2b63a9f4a307e6863d113eb5
myogin <yoginugraha19@gmail.com>
2023-08-15 19:05:13 +0800

    tes header

--- next.config.js
+++ next.config.js
@@ -5,21 +5,10 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://203.85.37.54:8000/:path*",
 
         // matching all API routes
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
         ],
       },
     ];
