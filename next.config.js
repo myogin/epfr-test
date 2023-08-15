19b08d40557a94bbad212b836dd6b469cf0c1811
myogin <yoginugraha19@gmail.com>
2023-08-15 21:47:27 +0800

    revert

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://203.85.37.54:8000/api/:path*",
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
